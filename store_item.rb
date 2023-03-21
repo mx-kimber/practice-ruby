8d092a0a59a14e83f6a717ea1d3b5a66063a4e0d
Kimber Ogden <mx.kimber@gmail.com>
2023-03-21 15:01:51 -0500

    added variation of hash

--- store_item.rb
+++ store_item.rb
@@ -2,10 +2,10 @@
 #p tshirts
 
 tshirts ={"color" => "blue", "size" => "small", "price" => 25}
-p tshirts
+p "#{tshirts['color']}"
 
 pants = {color: "black", size: "medium", price: 60}
-p pants
+p pants[:size]
 
 shoes = {color: "brown", size: 8.5, price: 72}
 p shoes
