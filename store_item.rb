50524fba3ada4d34a395ead9438fc4eacbbfb0a8
Kimber Ogden <mx.kimber@gmail.com>
2023-03-21 14:31:54 -0500

    added new hash for tshirts

--- store_item.rb
+++ store_item.rb
@@ -1,4 +1,7 @@
-tshirts = {color: "blue", size: "small", price: 25}
+#tshirts = {color: "blue", size: "small", price: 25}
+#p tshirts
+
+tshirts ={"color" => "blue", "size" => "small", "price" => 25}
 p tshirts
 
 pants = {color: "black", size: "medium", price: 60}
@@ -6,3 +9,4 @@ p pants
 
 shoes = {color: "brown", size: 8.5, price: 72}
 p shoes
+
