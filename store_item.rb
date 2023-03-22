0d31d4a7f97a1b10d763089d43bc47d61063195e
Kimber Ogden <mx.kimber@gmail.com>
2023-03-22 12:21:29 -0500

    made a class instead of hash

--- store_item.rb
+++ store_item.rb
@@ -1,12 +1,56 @@
 #tshirts = {color: "blue", size: "small", price: 25}
 #p tshirts
 
-tshirts ={"color" => "blue", "size" => "small", "price" => 25}
-p "#{tshirts['color']}"
+#tshirts ={"color" => "blue", "size" => #"small", "price" => 25}
+#p "#{tshirts['color']}"
 
-pants = {color: "black", size: "medium", price: 60}
-p pants[:size]
+#pants = {color: "black", size: "medium", #price: 60}
+#p pants[:size]
 
-shoes = {color: "brown", size: 8.5, price: 72}
-p shoes
+#shoes = {color: "brown", size: 8.5, price: 72}
+#p shoes
+
+class Shirts
+
+  def initialize(input_color, input_size, input_price)
+    @color = input_color
+    @size = input_size
+    @price = input_price
+  end
+
+  # getter
+  def color
+    @color
+  end
+
+  def size
+    @size
+  end
+
+  def price
+    @price
+  end
+
+  # setter/writer
+  def color=(new_color)
+    @color = new_color
+  end
+
+  def size=(new_size)
+    @size = new_size
+  end
+
+  def price=(new_price)
+    @price = new_price
+  end
+
+  def print_info
+    "A #{color} shirt in a size #{size}: $#{price}"
+  end
+
+end
+
+shirt1 = Shirts.new("blue", "medium", "25")
+p shirt1.color
+p shirt1.print_info
 
