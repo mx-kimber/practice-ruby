84703cb709e6985db80f4039426ece760f8f880b
Kimber Ogden <mx.kimber@gmail.com>
2023-03-23 15:47:46 -0500

    first commit

---
+++ bowling.rb
@@ -0,0 +1,27 @@
+# player name
+# count turns/frames
+# make variable for score 1
+# make variable for score 2
+# add scores 1 & 2
+# store/shovel? into an array? hash?
+
+# add turn 2 totals
+# final score from turn 1 added to turn 2 totals for final turn score
+
+
+bowler1 = [ ]
+
+points1 = 5
+points2 = 5
+
+turn1 = points1 + points2
+bowler1 << turn1
+
+turn2 = points1 + points2 + turn1
+bowler1 << turn2
+p bowler1
+
+# each index is added from previous index
+# loop
+
+
